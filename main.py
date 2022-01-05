938a4d7b47d1d41d0c4e0cc1394d741f1878f890
suyong eum <suyongeum@gmail.com>
2022-01-06 01:00:56 +0900

    icon_download_complete

--- main.py
+++ main.py
@@ -387,21 +387,43 @@ def clean_web_scraping_db():
 
 def icons8com():
 
-    # url = "https://search.icons8.com/api/iconsets/v5/search?term=ability&token=eVahCKnqVkgFJTwJ5xZmXPL6JN2OV6TwfIauZXIf"
-    # headers={'x-api-key':'eVahCKnqVkgFJTwJ5xZmXPL6JN2OV6TwfIauZXIf'}
-    # resp = requests.get(url,headers=headers)
+    # https://freeicons.io/
+    # icons8.com
 
-    # print(resp.status_code)
-    # print(resp.content)
-    # print(resp)
+    # First
+    word = 'ability'
 
-    # url = "https://api-icons.icons8.com/publicApi/icons/icon?id=9bLZQZMri5Xi&token=eVahCKnqVkgFJTwJ5xZmXPL6JN2OV6TwfIauZXIf"
-    # headers={'x-api-key':'eVahCKnqVkgFJTwJ5xZmXPL6JN2OV6TwfIauZXIf'}
-    # resp = requests.get(url,headers=headers)
+    # Second: search ids
+    url = "https://search.icons8.com/api/iconsets/v5/search?term=" + word +"&token=eVahCKnqVkgFJTwJ5xZmXPL6JN2OV6TwfIauZXIf"
+    headers={'x-api-key':'eVahCKnqVkgFJTwJ5xZmXPL6JN2OV6TwfIauZXIf'}
+    resp = requests.get(url,headers=headers)
 
-    # print(resp.status_code)
-    # print(resp.content.icon.svg)
-    # print(resp) 
+    ids = []
+    for item in resp.json()["icons"]:
+        ids.append(item["id"])
+   
+    if resp.status_code == 200:
+        print('success')
+    else:
+        return 0
+
+
+    for image_id in ids:
+
+        #image_id = '9bLZQZMri5Xi'
+
+        url = "https://api-icons.icons8.com/publicApi/icons/icon?id=" + image_id + "&token=eVahCKnqVkgFJTwJ5xZmXPL6JN2OV6TwfIauZXIf"
+        headers={'x-api-key':'eVahCKnqVkgFJTwJ5xZmXPL6JN2OV6TwfIauZXIf'}
+        resp = requests.get(url,headers=headers)
+
+        if resp.status_code == 200:
+            image           = resp.json()["icon"]["svg"]
+            file_write      = basepath + 'images\\' + word  + "_" + image_id + '.svg'
+            f_w             = open(file_write, "w")
+            f_w.write(image)
+            f_w.close()        
+        else:
+            print("No matching icon: ", word)
 
     return 0
 
@@ -410,8 +432,8 @@ def generate_html():
     return 0
 
 # Press the green button in the gutter to run the script.
-# basepath   = 'D:\projects\ecards\\'         # HOME
-basepath   = 'F:\EDIC_project\ecards\\'     # WORK
+basepath   = 'D:\projects\ecards\\'         # HOME
+# basepath   = 'F:\EDIC_project\ecards\\'     # WORK
 
 if __name__ == '__main__':
 
